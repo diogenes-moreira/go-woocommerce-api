cd07a19105f92cc03caefa6c2012e61c31d1320f
Diogenes Moreira <diogenes.moreira@gmail.com>
2025-04-05 00:58:08 -0300

    Add Categories

--- categories.go
+++ categories.go
@@ -61,3 +61,77 @@ func (service *CategoriesService) List(opts *ListCategoriesParams) (*[]Category,
 
 	return categories, response, nil
 }
+
+func (service *CategoriesService) Update(categoryID string, category *Category) (*Category, *http.Response, error) {
+	url := "/products/categories/" + categoryID
+	req, _ := service.client.NewRequest("PUT", url, nil, category)
+
+	updatedCategory := new(Category)
+	response, err := service.client.Do(req, updatedCategory)
+
+	if err != nil {
+		return nil, response, err
+	}
+
+	return updatedCategory, response, nil
+}
+
+type DeleteCategoryParams struct {
+	Force string `json:"force,omitempty"`
+}
+
+func (service *CategoriesService) Delete(categoryID string, param *DeleteCategoryParams) (*http.Response, error) {
+	url := "/products/categories/" + categoryID
+	req, _ := service.client.NewRequest("DELETE", url, param, nil)
+
+	category := new(Category)
+	response, err := service.client.Do(req, category)
+
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
+
+func (service *CategoriesService) Create(category *Category) (*Category, *http.Response, error) {
+	url := "/products/categories"
+	req, _ := service.client.NewRequest("POST", url, nil, category)
+
+	createdCategory := new(Category)
+	response, err := service.client.Do(req, createdCategory)
+
+	if err != nil {
+		return nil, response, err
+	}
+
+	return createdCategory, response, nil
+
+}
+
+type BatchCategoryUpdate struct {
+	Create *[]Category `json:"create,omitempty"`
+	Update *[]Category `json:"update,omitempty"`
+	Delete *[]int      `json:"delete,omitempty"`
+}
+
+type BatchCategoryUpdateResponse struct {
+	Create *[]Category `json:"create,omitempty"`
+	Update *[]Category `json:"update,omitempty"`
+	Delete *[]Category `json:"delete,omitempty"`
+}
+
+func (service *CategoriesService) Batch(opts *BatchCategoryUpdate) (*BatchCategoryUpdateResponse, *http.Response, error) {
+	url := "/products/categories/batch"
+	req, _ := service.client.NewRequest("POST", url, nil, opts)
+
+	batchCategory := new(BatchCategoryUpdateResponse)
+	response, err := service.client.Do(req, batchCategory)
+
+	if err != nil {
+		return nil, response, err
+	}
+
+	return batchCategory, response, nil
+
+}
